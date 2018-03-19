913acb222847c916928d4b9bcf1bf54df9e4998c
srinivasmeenavalli <srinivasmeenavalli@gmail.com>
2018-03-19 20:18:44 +0530

    changes

--- index.js
+++ index.js
@@ -29,50 +29,71 @@ restService.post("/webhook", function (req, res) {
     if (result) {
       console.log("result" + result.templateTypes);
       return res.json({
-        "speech": "",
         "facebook": {
           "attachment": {
             "type": "template",
             "payload": {
-              "template_type": "list",
+              "template_type": "generic",
               "elements": [
                 {
-                  "title": "Classic T-Shirt Collection",
-                  "image_url": "https://xvir.github.io/img/apiai.png",
-                  "subtitle": "See all our colors",
+                  "title": "Smurfs: The Lost Village (2017)",
+                  "image_url": "https://www.moovrika.com/ext/makeimg.php?tbl=movies&id=15666&img=1&type=image&movie=Smurfs+The+Lost+Village&fs=400",
+                  "subtitle": "Smurfette attempts to find her purpose in the village. When she encounters a creature in the Forbidden Forest who drops a mysterious map, she sets off with her friends Brainy, Clumsy, and Hefty on an adventure to find the Lost Village before the evil wizard Gargamel does.",
                   "default_action": {
                     "type": "web_url",
-                    "url": "https://xvir.github.io/"
-                  
+                    "url": "https://www.moovrika.com/m/15666",
+                    "webview_height_ratio": "tall"
                   },
                   "buttons": [
                     {
-                      "title": "View",
+                      "title": "more info",
                       "type": "web_url",
-                      "url": "https://xvir.github.io/"
+                      "url": "https://www.moovrika.com/m/4082",
+                      "webview_height_ratio": "tall"
+                    },
+                    {
+                      "title": "View trailer",
+                      "type": "web_url",
+                      "url": "https://www.moovrika.com/m/4082",
+                      "webview_height_ratio": "tall"
                     }
                   ]
                 },
                 {
-                  "title": "Classic T-Shirt Collection",
-                  "image_url": "https://xvir.github.io/img/apiai.png",
-                  "subtitle": "See all our colors",
+                  "title": "Resident Evil: The Final Chapter (2017)",
+                  "image_url": "https://www.moovrika.com/ext/makeimg.php?tbl=movies&id=4167&img=1&type=image&movie=Resident+Evil+The+Final+Chapter&fs=400",
+                  "subtitle": "Resident Evil: The Final Chapter is an upcoming science fiction action horror film written and directed by Paul W. S. Anderson. It is the sequel to Resident Evil: Retribution (2012), and will be the sixth and final installment in the Resident Evil film series, which is very loosely based on the Capcom survival horror video game series Resident Evil.",
                   "default_action": {
                     "type": "web_url",
-                    "url": "http://xvir.github.io/"
+                    "url": "https://www.moovrika.com/m/4167",
+                    "webview_height_ratio": "tall"
                   },
                   "buttons": [
                     {
-                      "title": "View",
+                      "title": "more info",
                       "type": "web_url",
-                      "url": "https://xvir.github.io/"
+                      "url": "https://www.moovrika.com/m/4082",
+                      "webview_height_ratio": "tall"
                     }
                   ]
                 }
               ]
             }
+
           }
-        } 
+
+        },
+        "kik": {
+          "type": "",
+          "body": ""
+        },
+        "slack": {
+          "text": "",
+          "attachments": []
+        },
+        "telegram": {
+          "text": ""
+        }
       });
     }
   });
